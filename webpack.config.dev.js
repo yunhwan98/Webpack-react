834cef31294b5d81e8c46a0230dd747dffb22e29
yunhwan <sky10240107@naver.com>
2023-03-21 14:12:09 +0900

    chore: webpack Mode 설정 - dev, prod

---
+++ webpack.config.dev.js
@@ -0,0 +1,10 @@
+//webpack.config.dev.js
+const { merge } = require("webpack-merge");
+const baseConfig = require("./webpack.config.base");
+
+module.exports = merge(baseConfig, {
+  mode: "development",
+  devServer: {
+    port: 3001,
+  },
+});
---
+++ webpack.config.prod.js
@@ -0,0 +1,7 @@
+//webpack.config.prod.js
+const { merge } = require("webpack-merge");
+const baseConfig = require("./webpack.config.base");
+
+module.exports = merge(baseConfig, {
+  mode: "production",
+});
