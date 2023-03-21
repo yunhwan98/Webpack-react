0e9b4ff6467c18226ab41c3bf1ae66fdc9d9c2db
yunhwan <sky10240107@naver.com>
2023-03-21 13:41:28 +0900

    init: 웹팩으로 React 구현

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>웹팩을 이용한 React 프로젝트</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+module.exports = {
+  name: "webpack-setting",
+  mode: "development", // 실서비스 : production
+  devtool: "eval", // 빠르게
+
+  entry: {
+    //제일 중요!
+    app: ["./src/index.jsx"],
+  }, // 입력
+  output: {
+    path: path.join(__dirname, "dist"), // 현재 폴더의 dist폴더에 생성
+    filename: "app.js",
+  }, // 출력
+  // entry에 있는 파일을 읽고 module을 적용한 후 output
+  module: {
+    //모듈 연결 설정
+    rules: [
+      {
+        test: /.(js|jsx)$/, // 대상 설정 정규식
+        loader: "babel-loader",
+        options: {
+          presets: ["@babel/preset-env", "@babel/preset-react"],
+        },
+      },
+      {
+        test: /.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "src", "index.html"),
+    }),
+  ],
+};
